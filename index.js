20e794628c1a3ba1b572cccfb78a3e2dcd2b1e07
AshishRadonfn-rad-49AOH <ashishranjan510360@gmail.com>
2022-10-28 10:18:37 +0530

    first comment

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const route = require("./src/routes/route")
+const app = express()
+const {multererror} =require("./src/multer-error/error")
+const cors = require("cors")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('dotenv').config()
+
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+.then(() => console.log("MongoDB is Connected..."))
+.catch(err => console.log(err))
+app.use(cors())
+app.use("/",route)
+app.use(multererror)
+app.listen(process.env.PORT, ()=>
+    console.log("Express App Running On Port 3000")
+)
